23b4fad2cf6804d6a89f803ec50258ab57807535
J. Victor Ferreira <victorf@kainos.com>
2019-01-25 17:41:21 +0000

    UpdateCaseTest passing

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/FormatDivorceSessionToCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/FormatDivorceSessionToCaseData.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.client.CaseFormatterClient;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
@@ -23,18 +21,10 @@ public class FormatDivorceSessionToCaseData implements Task<Map<String, Object>>
     }
 
     @Override
-    public Map<String, Object> execute(TaskContext context, Map<String, Object> payload) throws TaskException {
-        String selectedCourt = (String) context.getTransientObject("selectedCourt");
-        if (selectedCourt == null) {
-            throw new TaskException("Could not find selected court.");
-        }
-
-        HashMap<String, Object> payloadCopy = new HashMap<>(payload);
-        payloadCopy.put("courts", selectedCourt);//TODO - constant
-
+    public Map<String, Object> execute(TaskContext context, Map<String, Object> payload) {
         return caseFormatterClient.transformToCCDFormat(
                 context.getTransientObject(AUTH_TOKEN_JSON_KEY).toString(),
-                payloadCopy
+                payload
         );
     }
 
--- src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/FormatDivorceSessionToCaseDataTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/FormatDivorceSessionToCaseDataTest.java
@@ -11,18 +11,14 @@ import org.mockito.junit.MockitoJUnitRunner;
 import uk.gov.hmcts.reform.divorce.orchestration.client.CaseFormatterClient;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.DefaultTaskContext;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
-import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskException;
 
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
-import static org.hamcrest.Matchers.hasEntry;
 import static org.junit.Assert.assertEquals;
 import static org.junit.rules.ExpectedException.none;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.divorce.orchestration.TestConstants.AUTH_TOKEN;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.AUTH_TOKEN_JSON_KEY;
@@ -50,26 +46,9 @@ public class FormatDivorceSessionToCaseDataTest {
     }
 
     @Test
-    public void shouldAddSelectedCourtToPayload_AndCallCaseFormatterClientTransformToCCDFormat() throws TaskException {
+    public void shouldCallCaseFormatterClientTransformToCCDFormat() {
         when(caseFormatterClient.transformToCCDFormat(eq(AUTH_TOKEN), any())).thenReturn(testData);
         context.setTransientObject("selectedCourt", "randomlySelectedCourt");
-
-        assertEquals(testData, formatDivorceSessionToCaseData.execute(context, testData));
-
-        verify(caseFormatterClient).transformToCCDFormat(eq(AUTH_TOKEN), argThat(payload -> {
-            String selectedCourt = (String) payload.get("Hello");
-            return "World".equals(selectedCourt);
-        }));
-        verify(caseFormatterClient).transformToCCDFormat(eq(AUTH_TOKEN), argThat(payload -> {
-            String selectedCourt = (String) payload.get("courts");
-            return "randomlySelectedCourt".equals(selectedCourt);
-        }));
-    }
-
-    @Test
-    public void shouldThrowExceptionIfCourtWasNotSelected() throws TaskException {
-        expectedException.expect(TaskException.class);
-        expectedException.expectMessage("Could not find selected court.");
 
         assertEquals(testData, formatDivorceSessionToCaseData.execute(context, testData));
     }
