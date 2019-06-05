ad54f2b34b5328d94867c198a87f26ab53d2a7b7
J. Victor Ferreira <victor.ferreira@hmcts.net>
2019-06-05 16:41:38 +0100

    I think I'm finished

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/DefineWhoPaysCostsOrderTask.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/tasks/DefineWhoPaysCostsOrderTask.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.tasks;
 
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.Task;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.task.TaskContext;
 
@@ -7,11 +8,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.WHO_PAYS_CCD_CODE_FOR_RESPONDENT;
 import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.WHO_PAYS_COSTS_CCD_FIELD;
 
+@Component
 public class DefineWhoPaysCostsOrderTask implements Task<Map<String, Object>> {
-
-    private static final String WHO_PAYS_CCD_CODE_FOR_RESPONDENT = "respondent";
 
     @Override
     public Map<String, Object> execute(TaskContext context, Map<String, Object> payload) {
