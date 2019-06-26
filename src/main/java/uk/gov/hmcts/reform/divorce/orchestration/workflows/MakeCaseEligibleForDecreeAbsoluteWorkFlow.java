91ced20a94fd43c4809540b0ec75b9d5ffbf20c0
J. Victor Ferreira <victor.ferreira@hmcts.net>
2019-06-26 12:04:02 +0100

    More progress on workflow tests.

--- src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/MakeCaseEligibleForDecreeAbsoluteWorkFlow.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/orchestration/workflows/MakeCaseEligibleForDecreeAbsoluteWorkFlow.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.orchestration.workflows;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.DefaultWorkflow;
 import uk.gov.hmcts.reform.divorce.orchestration.framework.workflow.WorkflowException;
@@ -8,23 +9,27 @@ import uk.gov.hmcts.reform.divorce.orchestration.tasks.UpdateCaseInCCD;
 
 import java.util.Map;
 
+import static java.util.Collections.emptyMap;
+import static uk.gov.hmcts.reform.divorce.orchestration.domain.model.OrchestrationConstants.CASE_EVENT_ID_JSON_KEY;
+
 @Component
 public class MakeCaseEligibleForDecreeAbsoluteWorkFlow extends DefaultWorkflow<Map<String, Object>> {
 
+    private static final String MAKE_CASE_ELIGIBLE_FOR_DA_EVENT_ID = "makeCaseEligibleForDA";
     private UpdateCaseInCCD updateCaseInCCD;
 
     public MakeCaseEligibleForDecreeAbsoluteWorkFlow(UpdateCaseInCCD updateCaseInCCD) {
-
         this.updateCaseInCCD = updateCaseInCCD;
     }
 
     public Map<String, Object> run(String caseId, String authToken) throws WorkflowException {
-        this.execute(
-                new Task[]{
-                        updateCaseInCCD
-                },null
+        return this.execute(
+            new Task[] {
+                updateCaseInCCD
+            },
+            emptyMap(),
+            ImmutablePair.of(CASE_EVENT_ID_JSON_KEY, MAKE_CASE_ELIGIBLE_FOR_DA_EVENT_ID)
         );
-
-        return null;
     }
+
 }
